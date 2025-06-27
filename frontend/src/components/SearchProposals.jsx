f5a2148af4d4d373b4080adbe462c0b4679b6e12
Sukhdev Singh <117381583+Sukhdevsingh123@users.noreply.github.com>
2025-06-27 11:25:38 +0530

    Update SearchProposals.jsx

--- frontend/src/components/SearchProposals.jsx
+++ frontend/src/components/SearchProposals.jsx
@@ -27,7 +27,7 @@ const SearchProposals = () => {
 
     try {
       const response = await axios.get(
-        `https://civicgpt-tak.onrender.com/api/search_proposals/${encodeURIComponent(
+        `https://civicgpt.onrender.com/api/search_proposals/${encodeURIComponent(
           searchQuery
         )}`
       );
